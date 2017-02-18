fa73732497baefc3cdf22087bab38bc61d8fa7e0
Danilo Lima <danilofive@gmail.com>
2017-02-18 11:49:51 -0200

    WIP: Search for commands #1

--- cnote.py
+++ cnote.py
@@ -4,65 +4,81 @@ import boto3
 import uuid
 import time
 from os.path import expanduser
+from boto3.dynamodb.conditions import Attr
 import md5
 
 
-db = boto3.resource("dynamodb")
+class CNote:
 
-table = db.Table("CNoteHistory")
+    db = boto3.resource("dynamodb")
+    table = db.Table("CNoteHistory")
 
-''' Send history to server '''
+    def sendHistory(self, n):
 
+        home = expanduser("~")
+        f = open(home + "/.bash_history")
 
-def sendHistory(n):
+        print "Sending last", n, "entries ...!"
 
-    home = expanduser("~")
-    f = open(home + "/.bash_history")
+        with self.table.batch_writer() as batch:
 
-    print "Sending last", n, "entries ...!"
+            lines = self.tail(f, int(n)).split("\n")
+            groupId = str(uuid.uuid4())
 
-    with table.batch_writer() as batch:
+            for l in range(0, len(lines)):
 
-        lines = tail(f, int(n)).split("\n")
-        groupId = str(uuid.uuid4())
+                s_uuid = str(uuid.uuid4())
+                timestamp = int(time.time())
 
-        for l in range(0, len(lines)):
+                if (lines[l] != ''):
+                    batch.put_item(
+                        Item={
+                            'Uuid': s_uuid,
+                            'Timestamp': int(timestamp),
+                            'Command': str(lines[l]),
+                            'Sequence': int(l),
+                            'GroupId': groupId,
+                            'Enviroment': 'danilo@server-01',
+                            'Comment': 'Empty'
+                        }
+                    )
 
-            s_uuid = str(uuid.uuid4())
-            timestamp = int(time.time())
+        print "History Sent!"
 
-            if (lines[l] != ''):
-                batch.put_item(
-                    Item={
-                        'Uuid': s_uuid,
-                        'Timestamp': int(timestamp),
-                        'Command': str(lines[l]),
-                        'Sequence': int(l),
-                        'GroupId': groupId,
-                        'Enviroment': 'danilo@server-01',
-                        'Comment': 'Empty'
-                    }
-                )
-
-    print "History sent!"
-
-
-def help():
-    print "Help"
+    def help(elf):
+        print "Help"
 
 
-def tail(f, n):
-    return "\n".join(f.read().split("\n")[-n:])
+    def tail(self, f, n):
+        return "\n".join(f.read().split("\n")[-n:])
 
 
-def init(argv):
-    try:
-        n = argv[1]
-    except:
-        n = 100
+    def search(self, text):
+        response = self.table.scan(
+        Limit=100,
+        Select='SPECIFIC_ATTRIBUTES',
+        ReturnConsumedCapacity='TOTAL',
+        ProjectionExpression='Command, #TS, #S, Enviroment',
+        FilterExpression=Attr('Command').contains(text),
+        ExpressionAttributeNames={
+        '#TS': 'Timestamp', "#S": "Sequence"
+        },
+        ConsistentRead=False )
 
-    sendHistory(n)
+        for i in response['Items']:
+            print ">", i['Command']
+
+
+    def __init__(self, argv):
+
+        try:
+            n = argv[1]
+        except:
+            n = 100
+
+        #self.sendHistory(n)
+        self.search(n)
 
 
 if __name__ == "__main__":
-    init(sys.argv)
+    app = CNote(sys.argv);
